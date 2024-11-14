b8074dda1f066d43b6e6d200ffde281ae5377f69
testinhaGpe <Gabriel09deiz@hotmail.com>
2024-11-13 22:27:06 -0300

    Conexao

--- relatorios.php
+++ relatorios.php
@@ -1,17 +1,7 @@
 <?php
 // Conexão com o banco de dados
 
-//conexão Casa
-//$servername = "localhost"; // Endereço do servidor
-//$username = "root"; // Nome de usuário do banco de dados
-//$password = "psilva09"; // Senha do banco de dados
-//$dbname = "controleacesso_sql"; // Nome do banco de dados
-
-//Conexão Escola 
-$servername = "localhost:3308"; // Endereço do servidor
-$username = "root"; // Nome de usuário do banco de dados
-$password = "etec2024"; // Senha do banco de dados
-$dbname = "controleacesso_sql"; // Nome do banco de dados
+require('conexao.php'); // Conexão com o banco de dados
 
 // Criar a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
