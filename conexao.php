dbab03b139f07856a4e4c532b2d406bcadd03a52
testinhaGpe <Gabriel09deiz@hotmail.com>
2024-12-02 20:19:40 -0300

    up

--- conexao.php
+++ conexao.php
@@ -1,16 +1,16 @@
 <?php
 // Conexão com o banco de dados ----------------- Lembrando que na etec é localhost:3308 e senha etec2024
 
-//$servername = "localhost"; // Endereço do servidor
-//$username = "root"; // Nome de usuário do banco de dados
-//$password = "psilva09"; // Senha do banco de dados
-//$dbname = "controleacesso_sql"; // Nome do banco de dados
+$servername = "localhost"; // Endereço do servidor
+$username = "root"; // Nome de usuário do banco de dados
+$password = "psilva09"; // Senha do banco de dados
+$dbname = "controleacesso_sql"; // Nome do banco de dados
 
 //Conexão Escola 
-$servername = "localhost:3308"; // Endereço do servidor
-$username = "root"; // Nome de usuário do banco de dados
-$password = "etec2024"; // Senha do banco de dados
-$dbname = "controleacesso_sql"; // Nome do banco de dados
+//$servername = "localhost:3308"; // Endereço do servidor
+//$username = "root"; // Nome de usuário do banco de dados
+//$password = "etec2024"; // Senha do banco de dados
+//$dbname = "controleacesso_sql"; // Nome do banco de dados
 
 // Criar a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
